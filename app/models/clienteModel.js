475260f357d0f986ef75ba745dc31062010cf9e4
Samuel Rocha <samuelrodriguessr@hotmail.com>
2018-02-01 14:06:49 -0300

    models

---
+++ app/models/clienteModel.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+	this.all = function(){
+		return [
+			{nome:"Andre",email:"andre@email.com"},
+			{nome:"Paulo",email:"paulo@email.com"}
+		];
+	};
+	return this;
+};
--- app/routes/web.js
+++ app/routes/web.js
@@ -1,8 +1,9 @@
+var model = require('../models/clienteModel')();
 
 module.exports =  function(app){
 	
 	app.get('/', function(req,resp){
-		resp.render('site/home');
+		resp.render('site/home', {clientes:model.all()});
 	});
 
 	app.get('/contato', function(req,resp){
